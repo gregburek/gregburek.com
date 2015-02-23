3b962e51a7ac6530c36bcc948d465e2606532adf
Greg Burek <greg.burek@gmail.com>
2015-02-22 16:49:17 -0800

    Site updated to 7abcfc8

--- js/ga.js
+++ js/ga.js
@@ -1,16 +1,1 @@
-;(function ($, window, document, undefined) {
-  $(document).on('click', 'a', function() {
-    var href = $(this).attr('href');
-    var label = $(this).text()
-    var internalHost = new RegExp('/' + window.location.host + '/');
-
-    // Track outgoing links
-    if (!internalHost.test(href) && href.indexOf('http') == 0) {
-      _gaq.push(['_trackEvent', 'Outgoing Links', href, label]);
-    }
-
-    if (href.indexOf('mailto:') >= 0) {
-      _gaq.push(['_trackEvent', 'Mailto', href, label]);
-    }
-  });
-})( jQuery, window, document);
+!function(t,n,i){t(i).on("click","a",function(){var i=t(this).attr("href"),e=t(this).text(),o=new RegExp("/"+n.location.host+"/");o.test(i)||0!=i.indexOf("http")||_gaq.push(["_trackEvent","Outgoing Links",i,e]),i.indexOf("mailto:")>=0&&_gaq.push(["_trackEvent","Mailto",i,e])})}(jQuery,window,document);
